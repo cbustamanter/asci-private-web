77a1984f1e22b6063979f9b50349e2f952399d56
bigpun <cesar.raggio1@gmail.com>
2021-10-03 00:14:12 -0500

    avatar logout

--- src/components/intranet/StudentAvatar.tsx
+++ src/components/intranet/StudentAvatar.tsx
@@ -1,5 +1,14 @@
-import { Avatar } from "@chakra-ui/react";
+import {
+  Avatar,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import router from "next/router";
 import React from "react";
+import { useLogoutMutation } from "../../generated/graphql";
 import { S3_URL } from "../../utils/constant";
 import { useIsAuth } from "../../utils/useIsAuth";
 
@@ -7,7 +16,28 @@ interface StudentAvatarProps {}
 
 export const StudentAvatar: React.FC<StudentAvatarProps> = ({}) => {
   const { data } = useIsAuth();
+  const [, logout] = useLogoutMutation();
   const profileImg = data?.gender == 1 ? "boyprofile" : "girlprofile";
   const profileUrl = `${S3_URL}/public-assets/${profileImg}.png`;
-  return <Avatar h="32px" w="32px" src={profileUrl} bg="blue.500" />;
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<Avatar h="32px" w="32px" src={profileUrl} bg="blue.500" />}
+        colorScheme="white"
+        variant="unstyled"
+      />
+      <MenuList>
+        <MenuItem
+          onClick={async () => {
+            await logout();
+            router.replace("/login");
+          }}
+        >
+          Cerrar sesión
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
 };
